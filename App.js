c6c38b2ad5ddb1c6094e8985db3bd82e7f40c9c2
Karan Kumar <54062421+karankumarshreds@users.noreply.github.com>
2020-11-29 03:17:59 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,35 +2,68 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, TextInput, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
+const App = () => {
+  const [name, setName] = useState('');
+  const [names, setNames] = useState([]);
+  // save names 
+  const saveNames = () => {
+    setNames(oldArray => [name, ...oldArray]);
+    setName('');
+  }
+    return (
+      <View style={styles.app}>
+        <TextInput placeholder="Enter your name here..."
+          onChangeText={(val) => setName(val)}
+          value={name}
           style={styles.textInput}
-          placeholder="Add your task"
         />
-        <Button title="Save" color="#fc005d" />
+        <Button title="Save" onPress={saveNames}/>
+        {
+          names.map(each => <Text style={styles.eachName}>{each}</Text>)
+        }
       </View>
-    </View>
-  );
-}
-
+    );
+  }
 const styles = StyleSheet.create({
-  container: {
-    padding: 30,
+  app: {
+    marginHorizontal: "auto",
+    maxWidth: 500
+  },
+  logo: {
+    height: 80
+  },
+  header: {
+    padding: 20
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: "1.5rem",
+    marginVertical: "1em",
+    textAlign: "center"
+  },
+  text: {
+    lineHeight: "1.5em",
+    fontSize: "1.125rem",
+    marginVertical: "1em",
+    textAlign: "center"
   },
   textInput: {
-    borderColor: '#ededed',
-    borderWidth: 1,
     padding: 10,
-    paddingRight: 20,
-    paddingLeft: 20,
-    width: '70%'
+    borderColor: "black",
+    borderWidth: 1
   },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+  link: {
+    color: "#1B95E0"
   },
+  code: {
+    fontFamily: "monospace, monospace"
+  },
+  eachName: {
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    borderColor: "grey",
+    borderWidth: 1
+  }
 });
+
+export default App;
